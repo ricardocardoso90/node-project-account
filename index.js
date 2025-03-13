d41dc9e5c2ac068b1b37dccfa5847507de55a74a
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-03-13 14:51:03 -0300

    update commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const path = require('path');
-const extension = path.extname('script.js');
+const x = 35;
+const y = "Ricardo";
+const z = [1, 2, 3];
 
-console.log(extension);
+console.log(x, y, z);
+console.log("O nome é %s, e ele é programador!!", y);
+
+setTimeout(() => {
+  console.clear();
+}, 2000);
