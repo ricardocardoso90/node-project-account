632d9706e8316e33ca8cfa5a86f056a0914d601f
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-03-13 01:06:50 -0300

    update commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const fs = require('fs');
-fs.readFile('text.txt', 'utf8', (error, data) => {
-  error ? console.log(error) : console.log(data);
-});
+// export const fs = require('fs');
+// fs.readFile('text.txt', 'utf8', (error, data) => {
+//   error ? console.log(error) : console.log(data);
+// });
+
+export function nomeCompleto(nome, sobrenome) {
+  return console.log(`${nome} ${sobrenome}`);
+};
+
+nomeCompleto('Ricardo', 'Cardoso');
---
+++ script.js
@@ -0,0 +1,4 @@
+// import { nomeCompleto } from "./index.js";
+const nome = required('./index.js');
+
+nome.nomeCompleto('Rodrigo', 'Cardoso');
