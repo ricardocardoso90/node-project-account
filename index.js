a40d7541ce6196b40bd14106e27007c54283b95c
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-03-14 17:08:43 -0300

    update commit

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const x = 35;
-const y = "Ricardo";
-const z = [1, 2, 3];
+const fs = require("fs");
+const http = require("http");
 
-console.log(x, y, z);
-console.log("O nome é %s, e ele é programador!!", y);
+const novoArquivo = "novo-arquivo.txt";
 
-setTimeout(() => {
-  console.clear();
-}, 2000);
+fs.rename('arquivo.txt', 'novo-arquivo.txt', (error) => {
+  error
+    ? console.error(error)
+    : console.log(`Arquivo renomeado com sucesso para ${novoArquivo}`);
+});
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+
+  const name = "Ricardo Cardoso!!";
+
+  res.end(`<h1>Olá ${name}</h1>`);
+});
+
+server.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000!!");
+});
