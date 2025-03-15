ecfa3804e8b090b5d7cfcec9d9dcee6348e326a9
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-03-15 16:50:10 -0300

    update commit

--- index.js
+++ index.js
@@ -1,64 +1,39 @@
-const fs = require("fs");
-const http = require("http");
-const path = require("path");
-const os = require("os");
-const _ = require("lodash");
+import fs from "fs";
+import chalk from "chalk";
+import inquirer from "inquirer";
 
-const a = [1, 2, 4, 6, 8];
-const b = [2, 6, 9, 10, 12];
+console.log("Iniciando o projeto: Account");
 
-const diffa = _.difference(a, b);
-const diffb = _.difference(b, a);
-console.log(diffa);
-console.log(diffb);
+function operation() {
+  inquirer.prompt([{
+    type: 'list',
+    name: 'action',
+    message: 'O que você deseja fazer?',
+    choices: ['Criar uma conta', 'Consultar saldo', 'Depositar', 'Sacar', 'Sair'],
+  }])
+    .then(response => {
+      switch (response.action) {
+        case 'Criar uma conta':
+          createAccount();
+          break;
+        case 'Consultar saldo':
+          checkBalance();
+          break;
+        case 'Depositar':
+          deposit();
+          break;
+        case 'Sacar':
+          withdraw();
+          break;
+        case 'Sair':
+          console.log(chalk.red('Até mais!'));
+          break;
+        default:
+          console.log(chalk.red('Opção inválida'));
+          break;
+      }
+    })
+    .catch(error => console.log(error));
+};
 
-// console.log(os.cpus());
-console.log(os.freemem());
-console.log(os.homedir());
-console.log(os.type());
-
-const customPath = '/aulas/programacao/rocketseat/nodejs/node_teste.js';
-
-console.log(path.dirname(customPath));
-console.log(path.basename(customPath));
-
-!fs.existsSync('./minha_pasta')
-  ? (
-    console.log("Pasta não existe"),
-    fs.mkdirSync('./minha_pasta')
-  )
-  : console.log("Pasta já existe");
-
-const novoArquivo = "novo-arquivo.txt";
-
-fs.rename('arquivo.txt', novoArquivo, (error) => {
-  error
-    ? console.error(error)
-    : console.log(`Arquivo renomeado com sucesso para ${novoArquivo}`);
-});
-
-const arquivoDeTeste = "novo-arquivo.txt";
-
-fs.stat(arquivoDeTeste, (error, stats) => {
-  error
-    ? console.log(error)
-    : (
-      console.log(stats.isFile()),
-      console.log(stats.isDirectory()),
-      console.log(stats.isSymbolicLink()),
-      console.log(stats.size),
-      console.log(stats.birthtime)
-    );
-})
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-
-  const name = "Ricardo!!";
-  res.end(`<h1>Olá ${name}</h1>`);
-});
-
-server.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000!!");
-});
+operation();
